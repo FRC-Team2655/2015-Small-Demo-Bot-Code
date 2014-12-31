dea98a270349f8815a3f37933a86d96f61dd4f65
Hotdog Rocket <ninjosh97@gmail.com>
2014-12-31 14:08:30 -0500

    Changed turret methods

--- CRIO/src/edu/wpi/first/wpilibj/templates/Turret.java
+++ CRIO/src/edu/wpi/first/wpilibj/templates/Turret.java
@@ -8,38 +8,36 @@ import edu.wpi.first.wpilibj.Relay;
  * @author Zephan
  */
 public class Turret {
+
     private DigitalInput backLeftLimitSwitch;
     private DigitalInput backRightLimitSwitch;
-    
-    //Limit Switch right
-    private Relay spike; 
-    
-    public Turret(){
+
+    private Relay spike;
+
+    public Turret() {
         Relay spike = new Relay(Ports.turretRotationMotorChannel);
-        
+
         backLeftLimitSwitch = new DigitalInput(Ports.backLeftTurretLimitSwitchChannel);
         backRightLimitSwitch = new DigitalInput(Ports.backLeftTurretLimitSwitchChannel);
-        
-    }
-    
-    
-    void turnLeft(boolean direction){
-            if(!backLeftLimitSwitch.get()){
-                spike.set(Relay.Value.kOn);
-                spike.set(Relay.Value.kReverse);
-            }
-        
 
-        
     }
-    void turnRight(){
-            if(!backRightLimitSwitch.get()){
-                spike.set(Relay.Value.kOn);
-                spike.set(Relay.Value.kForward);
-            }
+
+    void turnLeft(boolean direction) {
+        if (!backLeftLimitSwitch.get()) {
+            spike.set(Relay.Value.kOn);
+            spike.set(Relay.Value.kReverse);
+        }
+
     }
-    
-    void turnOff(){
+
+    void turnRight() {
+        if (!backRightLimitSwitch.get()) {
+            spike.set(Relay.Value.kOn);
+            spike.set(Relay.Value.kForward);
+        }
+    }
+
+    void turnOff() {
         spike.set(Relay.Value.kOff);
     }
 }
