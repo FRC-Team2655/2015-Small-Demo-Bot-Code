7de1ffe5378c6b138d859d0a8bb73ae61c2af7a2
Shrinky <number45zephan@gmail.com>
2014-12-31 14:40:26 -0500

    Added the flywheel code

--- CRIO/src/edu/wpi/first/wpilibj/templates/FlyWheel.java
+++ CRIO/src/edu/wpi/first/wpilibj/templates/FlyWheel.java
@@ -10,16 +10,34 @@ public class FlyWheel {
 
     Talon flyWheel;
 
+    int speed;
+
     FlyWheel() {
         flyWheel = new Talon(Ports.flyWheelMotorChannel);
+        speed = 0;
     }
 
     public void spinUp() {
+        if (speed < 1 && speed > 0) {
+            speed++;
+        } else {
+            speed = 0;
+        }
+        flyWheel.set(speed);
+
     }
 
     public void spinDown() {
+        if (speed < 1 && speed > 0) {
+            speed--;
+        } else {
+            speed = 0;
+        }
+        flyWheel.set(speed);
     }
 
     public void turnOff() {
+        speed = 0;
+        flyWheel.set(speed);
     }
 }
