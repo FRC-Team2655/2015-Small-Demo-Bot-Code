dde8de7396a19f296361d1879656eb6e8d943adf
Shrinky <number45zephan@gmail.com>
2014-12-30 20:41:50 -0500

    Added the lifter code

--- CRIO/src/edu/wpi/first/wpilibj/templates/Lifter.java
+++ CRIO/src/edu/wpi/first/wpilibj/templates/Lifter.java
@@ -9,17 +9,23 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 public class Lifter {
 
     TeamDoubleSolenoid lifter;
+    TeamTimer timer;
 
     Lifter() {
         lifter = new TeamDoubleSolenoid(Ports.SM.ch1, Ports.SM.ch2);
         lifter.set(DoubleSolenoid.Value.kOff);
+        timer = new TeamTimer();
     }
 
     public void lift() {
         lifter.set(DoubleSolenoid.Value.kForward);
+        TeamTimer.delay(100);
+        lifter.set(DoubleSolenoid.Value.kOff);
     }
 
     public void lower() {
         lifter.set(DoubleSolenoid.Value.kReverse);
+        TeamTimer.delay(100);
+        lifter.set(DoubleSolenoid.Value.kOff);
     }
 }
